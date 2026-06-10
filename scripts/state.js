60f54a2371a0349b6aa2381d9bba19e0859250c5
ddmeer321 <shihengzhangdd@gmail.com>
2026-06-10 16:23:38 +0200

    Nerf Nova Shade

--- scripts/state.js
+++ scripts/state.js
@@ -1,4 +1,4 @@
-﻿import { loadCoins, loadHighScore, loadLeaderboard, loadProgression } from "./storage.js?v=companion2";
+﻿import { loadCoins, loadHighScore, loadLeaderboard, loadProgression } from "./storage.js?v=nova2";
 
 export function createState() {
   const highScore = loadHighScore();
