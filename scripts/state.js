f1f14285332480f6ed5d731362e8c048ac2eec1d
ddmeer321 <shihengzhangdd@gmail.com>
2026-06-10 15:21:11 +0200

    Fix German umlauts in shop text

--- scripts/state.js
+++ scripts/state.js
@@ -1,4 +1,4 @@
-import { loadCoins, loadHighScore, loadLeaderboard, loadProgression } from "./storage.js?v=companion1";
+import { loadCoins, loadHighScore, loadLeaderboard, loadProgression } from "./storage.js?v=companion2";
 
 export function createState() {
   const highScore = loadHighScore();
@@ -39,3 +39,4 @@ export function createState() {
     pickups: []
   };
 }
+
