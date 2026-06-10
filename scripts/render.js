fb8acc5ae16297f6761c7cc56329f522b89d7ced
ddmeer321 <shihengzhangdd@gmail.com>
2026-06-10 15:23:11 +0200

    Fix German umlaut encoding

--- scripts/render.js
+++ scripts/render.js
@@ -1,4 +1,4 @@
-import { companions, defaultCosmetic } from "./config.js?v=companion2";
+﻿import { companions, defaultCosmetic } from "./config.js?v=companion2";
 
 export function draw(dom, state) {
   const { canvas, ctx } = dom;
