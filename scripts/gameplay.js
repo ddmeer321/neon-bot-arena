87af1fcd967d4dbfaf0e5e57e5a93b8487617a10
ddmeer321 <shihengzhangdd@gmail.com>
2026-06-11 20:46:02 +0200

    Harden online leaderboard

--- scripts/gameplay.js
+++ scripts/gameplay.js
@@ -1,8 +1,8 @@
-﻿import { clamp, cleanName, distance } from "./utils.js";
-import { saveHighScore, saveLeaderboardEntry } from "./storage.js?v=sound2";
-import { addCoins, calculateCoinReward, getSelectedHeroStats } from "./economy.js?v=sound2";
-import { loadOnlineScores, submitOnlineScore } from "./online-leaderboard.js?v=leaderfilter1";
-import { playShoot, setMusicPaused, startMusic, stopMusic } from "./audio.js?v=sound2";
+import { clamp, cleanName, distance } from "./utils.js";
+import { saveHighScore, saveLeaderboardEntry } from "./storage.js?v=security2";
+import { addCoins, calculateCoinReward, getSelectedHeroStats } from "./economy.js?v=security2";
+import { loadOnlineScores, submitOnlineScore } from "./online-leaderboard.js?v=security2";
+import { playShoot, setMusicPaused, startMusic, stopMusic } from "./audio.js?v=security2";
 
 export function createGameplay({ dom, state, renderLeaderboard }) {
   const difficultySettings = {
