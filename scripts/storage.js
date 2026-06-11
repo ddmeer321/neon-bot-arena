34163588322610663f2075b3018690d917d6e31f
ddmeer321 <shihengzhangdd@gmail.com>
2026-06-11 20:25:28 +0200

    Tune audio volume and joystick knob

--- scripts/storage.js
+++ scripts/storage.js
@@ -1,4 +1,4 @@
-﻿import { coinKey, defaultCosmetic, highScoreKey, leaderboardKey, progressionKey, starterHeroes } from "./config.js?v=sound1";
+﻿import { coinKey, defaultCosmetic, highScoreKey, leaderboardKey, progressionKey, starterHeroes } from "./config.js?v=sound2";
 import { cleanName } from "./utils.js";
 
 export function loadHighScore() {
