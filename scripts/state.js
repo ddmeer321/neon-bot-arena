035f0a5c792faad091b173cd3aeffbb2004dfb32
ddmeer321 <shihengzhangdd@gmail.com>
2026-06-26 20:37:06 +0200

    Bump state storage import for coop respawn

--- scripts/state.js
+++ scripts/state.js
@@ -1,4 +1,4 @@
-import { loadCoins, loadHighScore, loadLeaderboard, loadProgression } from "./storage.js?v=cooprespawn1";
+import { loadCoins, loadHighScore, loadLeaderboard, loadProgression } from "./storage.js?v=cooprespawn2";
 
 export function createState() {
   const highScore = loadHighScore();
