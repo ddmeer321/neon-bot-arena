5d348efcc3164d47bed5d516d39000f91223e64c
ddmeer321 <shihengzhangdd@gmail.com>
2026-07-10 21:18:48 +0200

    Refresh test rewarded ad code

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-import { bootGame } from "./scripts/main.js?v=testdevtools2";
+import { bootGame } from "./scripts/main.js?v=testrewardad1";
 import { unlockTestLab } from "./scripts/test-lock.js?v=testlock3";
 
 await unlockTestLab();
