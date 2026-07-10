42ef5576c57f9d8538f1ccd998b587d15b84fd55
ddmeer321 <shihengzhangdd@gmail.com>
2026-07-10 21:20:18 +0200

    Refresh rewarded ad integration

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-import { bootGame } from "./scripts/main.js?v=testrewardad1";
+import { bootGame } from "./scripts/main.js?v=testrewardad2";
 import { unlockTestLab } from "./scripts/test-lock.js?v=testlock3";
 
 await unlockTestLab();
